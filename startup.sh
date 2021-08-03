a2fc94d5abf06cb5cd7d2e77eacf68e065788c8e
miro00 <ozvard765@gmail.com>
2021-08-03 16:11:19 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,7 +6,12 @@ if [ -f ~/.bash_aliases ]; then
   rm ~/.bash_aliases
 fi
 
+if [ -f ~/.gitconfig ]; then
+  rm ~/.gitconfig
+fi
+
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
 . ~/.bashrc
